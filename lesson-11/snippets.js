bb34768c775252e76a9957fc0e5765ecb43c6a48
LLikely <lynn@likelyarts.com>
2023-01-20 16:38:50 -0800

    initial commit

---
+++ lesson-11/snippets.js
@@ -0,0 +1,16 @@
+
+
+
+if (condition) {
+    //code to run if condition is true 
+} else {
+    //run some other code instead 
+}
+
+let bff = 'Brian';
+
+if (bff != 'Brian') {
+    console.log("Hi " + bff);
+} else {
+    console.log("Hello World");
+}
